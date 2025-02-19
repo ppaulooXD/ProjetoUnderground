5bf06cb5d978a75189f095762be0b53dc550b713
ph$ <ph.total12@gmail.com>
2025-02-19 09:05:15 -0300

    feats: implement de layout p paginas

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const routerHome = require("./routes/homeRoute");
+const expressEjsLayout = require("express-ejs-layouts");
 const app = express();
 
 app.use('/public', express.static(__dirname + "/public"));
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
+app.set("layout", "./layout.ejs");
+app.use(expressEjsLayout);
 app.use("/", routerHome);
 
 app.listen(5000, function(){
